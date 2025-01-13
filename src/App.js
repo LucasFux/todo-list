2583df9a821cf47bd5b4da60ea8bf1b2d2f96013
Lucas F <lucasfux08@gmail.com>
2025-01-13 17:58:11 -0300

    Se agregaron funcionalidades de completar items y de eliminarlos

--- src/App.js
+++ src/App.js
@@ -9,27 +9,49 @@ import {CreateTodoButton} from './CreateTodoButton.js'
 
 const defaultTodos = [
   {text: 'abc', completed: false},
-  {text: 'xyz', completed: false},
+  {text: 'xyz', completed: true},
   {text: 'bot', completed: true},
   {text: 'XYZ', completed: true}
 ]
 
 function App() {
- const [todos, setTodos] = React.useState(defaultTodos)
+  const [todos, setTodos] = React.useState(defaultTodos)
   const [searchValue, setSearchValue] = React.useState('')
-  console.log('tenemos: ' + todos)
+  const [counterTitle, setCounterTitle] = React.useState('')
 
   const totalTodos = todos.length
   const completedTodos = todos.filter(todo => !!todo.completed).length
 
-  const searchedTodos = todos.filter(todo => todo.text.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()))
+  const searchedTodos = todos.filter((todo) => {
+    const textTodo = todo.text.toLocaleLowerCase()
+    const textSearch = searchValue.toLocaleLowerCase() 
+    return textTodo.includes(textSearch)})
+
+    const onComplete = (text) => {
+      const todoIndex = todos.findIndex((todo) => todo.text === text);
+      const newTodos = [...todos];
+      newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+      setTodos(newTodos);
+      console.log("Todos: " + newTodos)
+    };
+    const deleteTodo = (text) => {
+      const newTodos = [...todos];
+      const todoIndex = newTodos.findIndex(
+        (todo) => todo.text === text
+      );
+      newTodos.splice(todoIndex, 1);
+      setTodos(newTodos);
+    };
 
   return (
     <React.Fragment>
       <TodoCounter
-        setTodos = {setTodos}
         completed={completedTodos}
-        total={totalTodos} />
+        total={totalTodos}
+        counterTitle = {counterTitle}
+        setCounterTitle = {setCounterTitle} 
+        
+        />
       <TodoSearch 
         searchValue = {searchValue}
         setSearchValue = {setSearchValue}
@@ -40,7 +62,10 @@ function App() {
           <TodoItem 
           key={todo.text} 
           text={todo.text}
-          completed={todo.completed} />
+          completed={todo.completed}
+          onComplete={() => onComplete(todo.text)}
+          onDelete={()=>deleteTodo(todo.text)}
+          />
         ))}
       </TodoList>
 
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,9 +1,15 @@
 import './TodoCounter.css';
 
 function TodoCounter({total, completed}){
+  if (total===completed){
+    return(
+      <h1>Completaste todas las tareas</h1>
+    )
+
+  } else{
     return(
       <h1>Completadas {completed} de {total} tareas</h1>
-    )
+    )}
   }
 
   export {TodoCounter};
